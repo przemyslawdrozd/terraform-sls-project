ecede7e531404338e0723a07246e4abf83e119fd
Przemo <przemo06133@gmail.com>
2023-11-19 19:53:29 +0100

    Add lambda code with zip archive

--- main.tf
+++ main.tf
@@ -13,14 +13,32 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-resource "aws_dynamodb_table" "my-table" {
-  name           = "dev-my-table"
-  hash_key       = "title"
-  read_capacity  = 5
-  write_capacity = 5
+resource "aws_iam_role" "my_lambda_role" {
+  name = "dev_lambda_dynamodb_role"
 
-  attribute {
-    name = "title"
-    type = "S"
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "dev_my_lambda"
+  handler       = "event-lambda.lambda_handler"
+  runtime       = "python3.8"
+  role          = aws_iam_role.my_lambda_role.arn
+  filename      = "./src/event-lambda.zip"
+}
+
+data "archive_file" "zip_lambda" {
+  type = "zip"
+  source_dir = "${path.module}/src"
+  output_path = "${path.module}/src/event-lambda.zip"
+
 }
