9e9e2dc271839e3ba63e3db2e9a33ca306d02f19
Przemo <przemo06133@gmail.com>
2023-11-20 22:19:15 +0100

    Init basic api resources

--- main.tf
+++ main.tf
@@ -11,6 +11,38 @@ terraform {
 
 provider "aws" {
   region  = "eu-central-1"
+
+resource "aws_api_gateway_rest_api" "my_rest_api" {
+  name        = "dev-rest-api"
+}
+
+resource "aws_api_gateway_resource" "my_api_resource" {
+  rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
+  parent_id   = aws_api_gateway_rest_api.my_rest_api.root_resource_id
+  path_part   = "api"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.my_rest_api.id
+  resource_id   = aws_api_gateway_resource.my_api_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "api_integration" {
+  rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
+  resource_id = aws_api_gateway_resource.my_api_resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.my_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.my_rest_api.execution_arn}/*/*"
 }
 
 resource "aws_iam_role" "my_lambda_role" {
